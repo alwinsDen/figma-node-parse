feffe185bc5053def9d3b52de4fcc912aec1de7b
alwinvarghese <jsmadesimple@gmail.com>
2025-01-03 22:11:34 +0530

    additional support.

--- formatext.js
+++ formatext.js
@@ -2,10 +2,13 @@ const fs = require("fs");
 const jsonFile=require("./main.json");
 const { rgbaToCssString, effectProccesor } = require("./supporter");
 const prssentty = jsonFile["nodes"]["1:180"]["document"];
+const cssMp=require("./mappedType.json");
+
+const treeMapXY=[];
 
 function treeMapper(parent,tabIndex){
     let rootString = `${'\t'.repeat(tabIndex)}-${parent["name"]}\n`;
-    let cssGenerator = `\n*/${parent["name"]}*/\n`;
+    let cssGenerator = `\n/* ${parent["name"]} */\n`;
     if(parent["backgroundColor"]){
         const bckclr=parent["backgroundColor"];
         cssGenerator+=rgbaToCssString(bckclr.r,bckclr.g, bckclr.b, bckclr.a);
@@ -13,18 +16,57 @@ function treeMapper(parent,tabIndex){
     if(parent["effects"]?.length>0){
         cssGenerator+=effectProccesor(parent["effects"]);
     }
+    if(parent["cornerRadius"]){
+        cssGenerator+=`${cssMp.borderRadius}:${parent["cornerRadius"]}px;\n`
+    }
+    if(parent["strokeAlign"]){
+        if(parent["strokeAlign"]==="INSIDE"){
+            cssGenerator+=`${cssMp.boxSizing}: border-box;\n`
+        }
+    }
+    if(parent["blendMode"]){
+        if(parent["blendMode"]==="NORMAL"){
+            cssGenerator+=`${cssMp.mixBlend}: normal;\n`
+        }
+    }
+    if(parent["opacity"]!==undefined && parent["opacity"]!==null){
+        cssGenerator+=`${cssMp.opacity}:${parent["opacity"]};\n`
+    }
+    if(parent["absoluteBoundingBox"]?.["width"]){
+        let sss = parent["absoluteBoundingBox"]["width"];
+        cssGenerator+=`${cssMp.width}:${sss}px;\n`
+    }
+    if(parent["absoluteBoundingBox"]?.["height"]){
+        let sss = parent["absoluteBoundingBox"]["height"];
+        cssGenerator+=`${cssMp.height}:${sss}px;\n`
+    }
+    //this gives us the x and y of the parent
+    if(parent["absoluteBoundingBox"]?.["x"]&&parent["absoluteBoundingBox"]?.["y"]&&treeMapXY.length===0){
+        treeMapXY.push({
+            x:parent["absoluteBoundingBox"]["x"],
+            y:parent["absoluteBoundingBox"]["y"]
+        })
+    }
+    if(parent["constraints"]){
+        if(parent["constraints"]?.["horizontal"]==="LEFT"||parent["constraints"]?.["horizontal"]==="RIGHT"){
+            if(parent["constraints"]?.["horizontal"]==="LEFT"){
+                cssGenerator+=`${cssMp.left}:${parent["absoluteBoundingBox"]["x"] - treeMapXY[0].x}px;\n`
+            }else{
+                // let scale = parent["absoluteRenderBounds"]["width"]/parent["absoluteBoundingBox"]["width"];
+                // cssGenerator+=`${cssMp.right}:${treeMapXY[0].x - parent["absoluteBoundingBox"]["x"]}px;\n`
+            }
+        }
+    }
     parent["children"]?.forEach((lls,inx)=> {
         rootString+=treeMapper(lls, tabIndex+1).rootString;
         cssGenerator+=treeMapper(lls, tabIndex+1).cssGenerator;
     })
     return {rootString, cssGenerator};
 }
-const {rootString,cssGenerator} = treeMapper(prssentty,0);
-
+const {rootString,cssGenerator} = treeMapper(prssentty,0,0);
 fs.writeFile("output.txt",`${rootString}\n\n${cssGenerator}`,(err)=> {
     if(err){
         throw err;
     }else{
-        console.log("file has been written.");
     }
 })
--- supporter.js
+++ supporter.js
@@ -2,24 +2,23 @@ const cssMp=require("./mappedType.json");
 
 const rgbaToCssString=(r, g, b, a)=> {
     let {blue,green,red,opacity}=rgbaProcess({r,g,b,a});
-    return `${cssMp.background}: rgba(${red}, ${green}, ${blue}, ${opacity});\n`;
+    return `${cssMp.background}:rgba(${red}, ${green}, ${blue}, ${opacity});\n`;
 }
 
 const effectProccesor=(kks)=>{
-    let cmpx = `${cssMp.boxShadow}: `
+    let cmpx = `${cssMp.boxShadow}:`
     kks.forEach((mss,inx)=>{
         if(mss.type==="DROP_SHADOW"&&mss.visible===true){
             let {blue,green,red,opacity}=rgbaProcess(mss.color);
             let z=mss?.radius;
             let {x,y} = mss?.offset;
             cmpx+=`${x}px ${y}px ${z}px rgba(${red}, ${green}, ${blue}, ${opacity})`
-            if(inx===kks.length-1){
-                cmpx+=";\n";
-            }else{
+            if(inx!==kks.length-1){
                 cmpx+=", ";
             }
         }
     })
+    cmpx+=";\n";
     return cmpx;
 }
 
